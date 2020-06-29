a9f3216db907fe601aaddbac82d6cf879730cf5c
Luiz Henrique Teixeira Mendes <luizhteixeiram@gmail.com>
2020-06-29 17:54:28 -0300

    adicionei versao paralela e algumas modificações na versao serial

--- main_paralelo.py
+++ main_paralelo.py
@@ -1,5 +1,6 @@
 import random
 import math
+import multiprocessing
 
 def generate_deck():
 	cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
@@ -69,6 +70,31 @@ def check_hand(hand):
 	base_score = (10000 * rankings[card_3[0]]) + (100 * rankings[card_2[0]]) + (1 * rankings[card_1[0]])
 	return base_score
 
+def play_single_game(i):	
+	deck = generate_deck()
+	random.shuffle(deck)
+
+	deck, player_hand = distribute_hand(deck)
+	player_score = check_hand(player_hand)
+
+	# Assuming player is playing optimal strategy, which is playing Q 6 4 or higher.
+	if player_score < 120604:
+		return 'fold'
+
+	deck, dealer_hand = distribute_hand(deck)
+	dealer_score = check_hand(dealer_hand)
+
+	if dealer_score < 120000:
+		return 'not qualified'
+	else:
+		if player_score > dealer_score:
+			return 'wins'
+		elif player_score == dealer_score:
+			return 'push'
+		else:
+			return 'lose'
+	
+
 if __name__ == '__main__':
 	balance = 100000
 	n_of_games = 1000000
@@ -77,36 +103,21 @@ if __name__ == '__main__':
 	print("Starting balance is: ${balance}".format(balance=balance))
 	print("Number of games: {n_of_games}".format(n_of_games=n_of_games))
 
-	for i in range(n_of_games):		
-		deck = generate_deck()
-		random.shuffle(deck)
+	pool = multiprocessing.Pool(multiprocessing.cpu_count())
+	results = pool.map(play_single_game, range(n_of_games))
 
-		deck, player_hand = distribute_hand(deck)
-		player_score = check_hand(player_hand)
+	pool.close()
 
-		# Assuming player is playing optimal strategy, which is playing Q 6 4 or higher.
-		if player_score < 120604:
-			# print("Player folds...")
+	for result in results:
+		if result == 'fold':
 			balance -= bet
-			continue
-
-		balance -= 2 * bet
-
-		deck, dealer_hand = distribute_hand(deck)
-		dealer_score = check_hand(dealer_hand)
-
-		if dealer_score < 120000:
-			# print("Dealer doesn't qualify")
-			balance += 3 * bet
+		elif result == 'not qualified':
+			balance += bet
+		elif result == 'wins':
+			balance += 2 * bet
+		elif result == 'push':
+			pass
 		else:
-			if player_score > dealer_score:
-				# print("Player wins")
-				balance += 4 * bet
-			elif player_score == dealer_score:
-				# print("Push")
-				balance += 2 * bet
-			else:
-				pass
-				# print("Dealer wins")
+			balance -= 2 * bet
 
 	print("\nFinal balance is ${balance}".format(balance=balance))
